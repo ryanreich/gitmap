392c6e70dbcf436cd1f91f627fb9af25f8ccac5e
Ryan Reich <ryan.reich@gmail.com>
2015-10-16 14:08:10 -0400

    Polish Handlers.hs

--- src/Handlers.hs
+++ src/Handlers.hs
@@ -14,6 +14,75 @@ import System.Process
 import FileNames
 import GitMapConfig
 
+handleGitOp :: String -> [String] -> GitMapRepoSpec -> ProcResult
+handleGitOp gitOp gitArgs repoSpec = do
+  (_, preOutput) <- preGitOp gitOp repoSpec
+  (gitCmd, gitOutput) <- doGitOp gitOp gitArgs repoSpec
+  (_, postOutput) <- postGitOp gitOp repoSpec
+
+  return (gitCmd, preOutput ++ gitOutput ++ postOutput)
+
+doGitOp :: String -> [String] -> GitMapRepoSpec -> ProcResult
+doGitOp "clone" gitArgs repoSpec =
+  basicGitRun' "." "clone" [gmrsURL repoSpec] gitArgs repoSpec
+doGitOp gitOp gitArgs repoSpec =
+  basicGitRun (gmrsName repoSpec) gitOp gitArgs repoSpec
+
+preGitOp :: String -> GitMapRepoSpec -> ProcResult
+preGitOp gitOp repoSpec =
+  let repoName = gmrsName repoSpec
+      repoOp = case gitOp of
+        "clone" -> gitSkipDir
+        "pull" -> gitCheckRemote
+        "fetch" -> gitCheckRemote
+        "push" -> gitCheckStatus
+        "commit" -> gitCheckStatus
+        "stash" -> gitCheckStatus
+        _ -> const noResult
+  in repoOp repoName
+
+gitSkipDir :: String -> ProcResult
+gitSkipDir dir = do
+  repoExists <- liftIO $ doesDirectoryExist dir
+  when repoExists quit
+
+gitCheckRemote :: String -> ProcResult
+gitCheckRemote dir = do
+  gitRun dir ["remote", "update"] ""
+  gitCheckStatus dir
+
+gitCheckStatus :: String -> ProcResult
+gitCheckStatus dir = do
+  (_, statOutput) <- gitRun dir ["status", "--porcelain"] ""
+  when (null statOutput) quit
+
+postGitOp :: String -> GitMapRepoSpec -> ProcResult
+postGitOp _ _ = noResult
+
+basicGitRun :: String -> String -> [String] -> GitMapRepoSpec -> ProcResult
+basicGitRun dir gitOp gitArgs = do
+  basicGitRun' dir gitOp [] gitArgs
+
+basicGitRun' :: String -> String -> [String] -> [String] -> GitMapRepoSpec -> ProcResult
+basicGitRun' dir gitOp gitArgs1 gitArgs2 repoSpec =
+  gitRun dir (gitOp:gitArgs1 ++ gitRepoArgs gitOp repoSpec ++ gitArgs2) ""
+
+gitRepoArgs :: String -> GitMapRepoSpec -> [String]
+gitRepoArgs gitOp repoSpec = HashMap.lookupDefault [] gitOp (gmrsGitArgs repoSpec)
+
+gitRun :: String -> [String] -> String -> ProcResult
+gitRun dir args stdIn = do
+  (exitCode, out, err) <-
+    liftIO $ readProcessWithExitCode gitExecName (["-C", dir] ++ args) stdIn
+    
+  let output = out ++ err
+      gitCmd = showCommandForUser gitExecName args
+      returnOp = case exitCode of
+        ExitSuccess -> return
+        _ -> throwE . Just
+        
+  returnOp (gitCmd, output)
+
 type ProcResult = ExceptT (Maybe (String, String)) IO (String, String)
 
 noResult :: ProcResult
@@ -30,65 +99,3 @@ whenQuitFailPass x y f g = case x of
   Left Nothing -> y
   Left (Just z) -> f z
   Right z -> g z
-
-handleGitOp :: String -> [String] -> GitMapRepoSpec -> ProcResult
-
-handleGitOp "clone" gitArgs repoSpec = do
-  let repoName = gmrsName repoSpec
-  repoExists <- liftIO $ doesDirectoryExist repoName
-  when repoExists quit
-  basicGitRun' "." "clone" [gmrsURL repoSpec] gitArgs repoSpec
-
-handleGitOp gitOp gitArgs repoSpec = do
-  let repoName = gmrsName repoSpec
-
-  (_, preOutput) <- preGitOp gitOp repoName repoSpec
-  (gitCmd, gitOutput) <- basicGitRun repoName gitOp gitArgs repoSpec
-  (_, postOutput) <- postGitOp gitOp repoName repoSpec
-
-  return (gitCmd, preOutput ++ gitOutput ++ postOutput)
-
-preGitOp :: String -> String -> GitMapRepoSpec -> ProcResult
-preGitOp "pull" = const . gitCheckRemote
-preGitOp "fetch" = const . gitCheckRemote
-preGitOp "push" = const . gitCheckStatus
-preGitOp "commit" = const . gitCheckStatus
-preGitOp "stash" = const . gitCheckStatus
-preGitOp _ = const . const noResult
-
-postGitOp :: String -> String -> GitMapRepoSpec -> ProcResult
-postGitOp _ _ = const noResult
-
-basicGitRun :: String -> String -> [String] -> GitMapRepoSpec -> ProcResult
-basicGitRun dir gitOp gitArgs = do
-  basicGitRun' dir gitOp [] gitArgs
-
-basicGitRun' :: String -> String -> [String] -> [String] -> GitMapRepoSpec -> ProcResult
-basicGitRun' dir gitOp gitArgs1 gitArgs2 repoSpec =
-  gitRun dir (gitOp:gitArgs1 ++ gitRepoArgs gitOp repoSpec ++ gitArgs2) ""
-
-gitCheckRemote :: String -> ProcResult
-gitCheckRemote dir = do
-  gitRun dir ["remote", "update"] ""
-  gitCheckStatus dir
-
-gitCheckStatus :: String -> ProcResult
-gitCheckStatus dir = do
-  (_, statOutput) <- gitRun dir ["status", "--porcelain"] ""
-  when (null statOutput) quit
-
-gitRun :: String -> [String] -> String -> ProcResult
-gitRun dir args stdIn = do
-  (exitCode, out, err) <-
-    liftIO $ readProcessWithExitCode gitExecName (["-C", dir] ++ args) stdIn
-  let output = out ++ err
-      gitCmd = showCommandForUser gitExecName args
-  when (exitFailed exitCode) $ throwE $ Just (gitCmd, output)
-  return (gitCmd, output)
-
-gitRepoArgs :: String -> GitMapRepoSpec -> [String]
-gitRepoArgs gitOp repoSpec = HashMap.lookupDefault [] gitOp (gmrsGitArgs repoSpec)
-
-exitFailed :: ExitCode -> Bool
-exitFailed ExitSuccess = False
-exitFailed _ = True
